68abe30cee9418701ec8a7880cc5880b72415f8a
Aluno <jeipi7007@gmail.com>
2025-05-05 10:32:08 -0300

    Commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>PixelPets - Teste</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Bem-vindo ao PixelPets!</h1>
+</body>
+</html>
